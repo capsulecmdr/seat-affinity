8db083ff078a7fb37fee83d6023ee577bdc1df6b
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-09-09 10:58:18 -0700

    .

---
+++ src/Http/Controllers/AffinityController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CapsuleCmdr\Affinity\Http\Controllers;
+
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+
+
+class AffinityController extends Controller
+{
+
+    public function about()
+    {
+        $user = Auth::user();
+
+        return view('affinity::about', compact('name'));
+    }
+}
--- src/Http/Routes.php
+++ src/Http/Routes.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use CapsuleCmdr\Affinity\Http\Controllers\AffinityController;
 
 Route::middleware(['web','auth'])
     ->prefix('affinity')
@@ -11,5 +12,5 @@ Route::middleware(['web','auth'])
             return response('This is a dummy route, nothing here yet.', 200);
         })->name('test');
 
-        Route::get('/about')->name('about');
+        Route::get('/about',[AffinityController::class,'about'])->name('about');
     });
