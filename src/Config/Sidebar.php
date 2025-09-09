f2de77c24cdd25eba057a03549f547079e4ed8c6
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-09-09 09:58:38 -0700

    .

--- src/Config/Sidebar.php
+++ src/Config/Sidebar.php
@@ -8,8 +8,32 @@ return [
         "permission"=>"affinity.admin",
         "entries"=>[
             [
-                "name"=>"Menu Test",
-                "icon"=>"fas fa-hexagon-nodes",
+                "name"=>"Intel Console",
+                "icon"=>"fas fa-light-emergency-on",
+                "route"=>"affinity.test",
+                "permission"=>"affinity.admin",
+            ],
+            [
+                "name"=>"Trust Center",
+                "icon"=>"fas fa-fingerprint",
+                "route"=>"affinity.test",
+                "permission"=>"affinity.admin",
+            ],
+            [
+                "name"=>"Entity Manager",
+                "icon"=>"fas fa-user-shield",
+                "route"=>"affinity.test",
+                "permission"=>"affinity.admin",
+            ],
+            [
+                "name"=>"Settings",
+                "icon"=>"fas fa-screwdriver-wrench",
+                "route"=>"affinity.test",
+                "permission"=>"affinity.admin",
+            ],
+            [
+                "name"=>"About",
+                "icon"=>"fas fa-info",
                 "route"=>"affinity.test",
                 "permission"=>"affinity.admin",
             ],
