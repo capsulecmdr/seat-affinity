c1f38c523f39593507d519ebacb868ab9a888c2c
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-09-09 09:15:15 -0700

    .

--- src/Resources/Lang/en/Permissions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'permission_affinity_admin_label' => 'Administrator',
-    'permission_affinity_admin_desc' => 'Grants full access to all administrative and configuration tools.',
-];
---
+++ src/Resources/Lang/en/permissions.old.php
@@ -0,0 +1,6 @@
+<?php
+//
+return [
+    'permission_affinity_admin_label' => 'Administrator',
+    'permission_affinity_admin_desc' => 'Grants full access to all administrative and configuration tools.',
+];
