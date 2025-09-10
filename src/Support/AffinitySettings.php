98bb285c23781a04fa43e48d7a32735cca1b9cbf
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-09-10 06:02:59 -0700

    .

--- src/Support/AffinitySettings.php
+++ src/Support/AffinitySettings.php
@@ -2,7 +2,7 @@
 
 namespace CapsuleCmdr\Affinity\Support;
 
-use CapsuleCmdr\Affinity\Database\Models\AffinitySetting;
+use CapsuleCmdr\Affinity\Models\AffinitySetting;
 use Illuminate\Support\Facades\Cache;
 
 class AffinitySettings
