924e324de739eed172e045da7a9d614a62292781
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-09-11 07:09:04 -0700

    .

--- src/Console/Commands/CheckTrustRelationships.php
+++ src/Console/Commands/CheckTrustRelationships.php
@@ -2,6 +2,7 @@
 
 namespace CapsuleCmdr\Affinity\Console\Commands;
 
+use CapsuleCmdr\Affinity\Models\AffinityEntity;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Seat\Eveapi\Models\Character\CharacterInfo;
@@ -36,7 +37,12 @@ class CheckTrustRelationships extends Command
                     $contacts = CharacterContact::where('character_id', $character->character_id)->get();
 
                     foreach ($contacts as $contact) {
-                        $trust = AffinityTrustRelationship::where('affinity_entity_id', $contact->contact_id)->first();
+                        $affinityEntity = AffinityEntity::where('eve_id',$contact->contact_id)->first();
+                        $trust = AffinityTrustRelationship::where('affinity_entity_id', $affinityEntity->id)->first();
+                        
+                        //default to neut
+                        
+                        
                         if ($trust && $trust->affinity_trust_class_id >= 3) {
                             $msg = sprintf(
                                 "User %d, Character %d, Contact %d has trust classification %d (>=3)",
