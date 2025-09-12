89b32db03ad627595ebaff3275b596eee8fdc681
That Guy <60452132+Gadgetwhir@users.noreply.github.com>
2025-09-12 07:25:30 -0700

    .

--- src/Http/Routes.php
+++ src/Http/Routes.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use CapsuleCmdr\Affinity\Http\Controllers\AffinityController;
 
+use Seat\Eseye\Eseye;
+use Seat\Eseye\Containers\EsiAuthentication;
+
 Route::middleware(['web','auth'])
     ->prefix('affinity')
     ->as('affinity.')
@@ -11,6 +14,29 @@ Route::middleware(['web','auth'])
         Route::get('/test', function () {
             return response('This is a dummy route, nothing here yet.', 200);
         })->name('test');
+
+        Route::get('/lab', function () {
+
+            $client_id = config('services.eveonline.client_id');
+            $client_secret = config('services.eveonline.client_secret');
+
+            $char_id = 2117189532;
+            $rt = \Seat\Eveapi\Models\RefreshToken::where('character_id',$char_id)->first();
+
+            $auth = new EsiAuthentication([
+            'client_id'  => $client_id,
+            'secret' => $client_secret,
+            'refresh_token' => $rt->token,
+            // 'access_token' optional; Eseye will fetch one using the refresher
+            // 'scopes' optional; Eseye can work without providing this
+            ]);
+
+            $esi = new Eseye($auth);
+
+            $me = $esi->invoke('get','/characters/{character_id}/', ['character_id' => $char_id]);
+
+            return $me;
+        })->name('lab');
 
         Route::get('/entities', [AffinityController::class, 'entityManager'])->name('entities.index');
         Route::post('/entities/trust', [AffinityController::class, 'updateTrust'])->name('entities.updateTrust');
